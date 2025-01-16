d95917e5b3bf2485213326c76c51e17fdc071eba
devla90 <luis_03_10@hotmail.com>
2025-01-16 12:22:32 -0500

    refactor: upgrade navigationwrapper

--- app/src/main/java/com/example/pokeapi/core/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/example/pokeapi/core/navigation/NavigationWrapper.kt
@@ -1,23 +1,19 @@
 package com.example.pokeapi.core.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.example.pokeapi.presentation.ui.screen.PokemonDetailScreen
 import com.example.pokeapi.presentation.ui.screen.PokemonListScreen
-import com.example.pokeapi.presentation.viewmodel.PokemonViewModel
 
 @Composable
 fun NavigationWrapper() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = PokemonList) {
         composable<PokemonList> {
-            val viewModel: PokemonViewModel = hiltViewModel()
             PokemonListScreen(
-                viewModel = viewModel,
                 onPokemonClick = { url ->
                     navController.navigate(PokemonDetail(url = url))
                 }
