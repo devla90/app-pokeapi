ef033fda02b3ac867097220cf8441f7a38edcad3
devla90 <luis_03_10@hotmail.com>
2025-01-16 15:21:05 -0500

    refactor: added sections in PokemonListScreen

--- app/src/main/java/com/example/pokeapi/presentation/ui/screen/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokeapi/presentation/ui/screen/PokemonListScreen.kt
@@ -1,12 +1,23 @@
 package com.example.pokeapi.presentation.ui.screen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material3.Button
@@ -18,12 +29,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pokeapi.data.model.Pokemon
 import com.example.pokeapi.domain.usecase.GetPokemonListUseCase
 import com.example.pokeapi.presentation.PokemonApp
 import com.example.pokeapi.presentation.viewmodel.PokemonViewModel
@@ -32,7 +49,6 @@ import com.example.pokeapi.presentation.viewmodel.UiState
 
 @Composable
 fun PokemonListScreen(
-    modifier: Modifier = Modifier,
     viewModel: PokemonViewModel = hiltViewModel(),
     onPokemonClick: (String) -> Unit
 ) {
@@ -52,50 +68,161 @@ fun PokemonListScreen(
             .fillMaxSize()
             .padding(all = 10.dp)
     ) {
-        OutlinedTextField(
-            value = text,
-            onValueChange = {
-                text = it
-                viewModel.filterText(text)
+        TitleSection()
+        SearchSection(
+            text = text,
+            onTextChanged = { newText ->
+                text = newText
             },
-            label = { Text("Pokemon") },
-            modifier = Modifier.fillMaxWidth()
+            onSearchClick = {
+                viewModel.filterPokemonByName(text)
+            }
         )
-        Button(onClick = {
-            viewModel.filterPokemonByName(text)
-        }) {
-            Text("Buscar")
-        }
 
         when (uiState) {
             is UiState.Loading -> {
-                CircularProgressIndicator()
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
             }
 
             is UiState.Error -> {
                 val errorMessage = (uiState as UiState.Error).message
-                Text("Error: $errorMessage", color = Color.Red)
+                Text(
+                    "Error: $errorMessage",
+                    color = Color.Red,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
             }
 
             is UiState.Success -> {
                 val pokemonMap = (uiState as UiState.Success).data
-                LazyColumn {
-                    items(pokemonMap.values.toList()) { pokemon ->
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(8.dp)
-                                .clickable { onPokemonClick(pokemon.url) }) {
-                            Text(
-                                pokemon.name,
-                                modifier = Modifier.padding(16.dp)
-                            )
-                        }
-                    }
-                }
+                ListSection(
+                    pokemonMap = pokemonMap,
+                    onPokemonClick
+                )
             }
 
             UiState.Idle -> {}
         }
     }
+}
+
+@Composable
+fun TitleSection() {
+    Text(
+        text = "App Pokemon",
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp)
+            .wrapContentHeight(Alignment.CenterVertically),
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTitleSection() {
+    TitleSection()
+}
+
+@Composable
+fun SearchSection(
+    text: String,
+    onTextChanged: (String) -> Unit,
+    onSearchClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(color = Color.Green)
+            .shadow(4.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = text,
+                onValueChange = onTextChanged,
+                label = {
+                    Text("Pokemon")
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+            )
+            Button(onClick = onSearchClick) {
+                Text("Search")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSearchSection() {
+    SearchSection(
+        text = "Bulbasaur",
+        onTextChanged = {},
+        onSearchClick = {}
+    )
+}
+
+@Composable
+fun ListSection(
+    pokemonMap: Map<String, Pokemon>,
+    onPokemonClick: (String) -> Unit
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(1),
+        //contentPadding = PaddingValues(8.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        items(pokemonMap.values.toList()) { pokemon ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .shadow(4.dp)
+                    //.border(2.dp, color = Color.Black, shape = RoundedCornerShape(24,24,24,24))
+                    .clickable {
+                        onPokemonClick(pokemon.url)
+                    },
+                border = BorderStroke(width = 1.dp, Color.Black),
+                elevation = 4.dp
+            ) {
+                Text(
+                    pokemon.name,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 16.dp),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewListSection() {
+    val pokemonMap = mapOf(
+        "bulbasaur" to Pokemon("bulbasaur", ""),
+        "charmander" to Pokemon("charmander", ""),
+        "squirtle" to Pokemon(name = "Squirtle", url = "")
+    )
+    ListSection(
+        pokemonMap = pokemonMap,
+        onPokemonClick = {}
+    )
 }
